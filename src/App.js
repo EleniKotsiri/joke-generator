5f18418da6e8f894c47869fc876ae9e505f1313c
Eleni Kotsiri <Eleni.Kotsiri@gr.ey.com>
2022-04-23 20:04:10 +0300

    set up joke generator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   const [joke, setJoke] = useState('');
+
+   useEffect(() => {
+      // fetchJoke makes an API call -> async
+      const fetchJoke = async () => {
+         await fetch(
+            `http://api.icndb.com/jokes/random`
+         )
+            .then(res => res.json())
+            .then(data => {
+               // console.log(data.value.joke);
+               setJoke(data.value.joke)
+            })
+      }
+
+      fetchJoke();
+   }, [])
+
+   return (
+      <div className="App">
+         <h1>Joke generator</h1>
+         <button>Generate joke</button>
+         <p>{joke}</p>
+      </div>
+   );
 }
 
 export default App;
---
+++ src/components/SimpleInput.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function SimpleInput(props) {
+   return (
+      <form>SimpleInput</form>
+   )
+}
+
+export default SimpleInput
